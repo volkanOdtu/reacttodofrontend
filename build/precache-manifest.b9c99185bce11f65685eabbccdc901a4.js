a4463d2016a9f87479e5ef9ea5e39262a55cf171
volkanOdtu <su_volkan@hotmail.com>
2019-10-14 21:18:19 +0300

    first

---
+++ build/precache-manifest.b9c99185bce11f65685eabbccdc901a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a2ea1a20b73a001ceaffedb6f576e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26da29aa80c67f1a7844",
+    "url": "/static/css/main.162d2f57.chunk.css"
+  },
+  {
+    "revision": "bf493b0b665c291338b8",
+    "url": "/static/js/2.39cc5a04.chunk.js"
+  },
+  {
+    "revision": "26da29aa80c67f1a7844",
+    "url": "/static/js/main.b30da99d.chunk.js"
+  },
+  {
+    "revision": "14b90be0eeb995d89cb1",
+    "url": "/static/js/runtime-main.e8a7bf87.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
